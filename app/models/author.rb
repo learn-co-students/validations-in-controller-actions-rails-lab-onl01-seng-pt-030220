4122b8235eb58853ff868d8161687c0bd69645a2
github username <github email address>
2020-07-20 16:15:35 -0600

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ActiveRecord::Base
+
+#name can't be blank
+#email is unique
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true
+
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,11 @@
 class Post < ActiveRecord::Base
+
+#title cannot be blank
+#category is either "Fiction" or "Non-Fiction"
+#content is at least 100 characters long
+    validates :title, presence: true
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validates :content, length: { minimum: 100 }
+
+
 end
